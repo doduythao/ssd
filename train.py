29dbadc2b81547e7a1aa9684fdd2b4a525afacb0
doduythao <doduythao@gmail.com>
2020-08-25 11:37:26 +0900

    fix train.py

--- train.py
+++ train.py
@@ -2,47 +2,47 @@ import time
 import torch.backends.cudnn as cudnn
 import torch.optim
 import torch.utils.data
+from model512 import SSD512
 from model import SSD300, MultiBoxLoss
-from datasets import PascalVOCDataset
+from datasets import *
 from utils import *
-
-# Data parameters
-data_folder = './'  # folder with data files
-keep_difficult = True  # use objects considered difficult to detect?
+import argparse
 
 # Model parameters
-# Not too many here since the SSD300 has a very specific structure
-n_classes = len(voc_label_map)  # number of different types of objects
+n_classes = len(coco_labels) + 1  # number of different types of objects
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Learning parameters
-checkpoint = None  # path to model checkpoint, None if none
-batch_size = 8  # batch size
 iterations = 120000  # number of iterations to train
 workers = 4  # number of workers for loading data in the DataLoader
 print_freq = 200  # print training status every __ batches
 lr = 1e-3  # learning rate
-decay_lr_at = [80000, 100000]  # decay learning rate after these many iterations
 decay_lr_to = 0.1  # decay learning rate to this fraction of the existing learning rate
 momentum = 0.9  # momentum
 weight_decay = 5e-4  # weight decay
-grad_clip = None  # clip if gradients are exploding, which may happen at larger batch sizes (sometimes at 32) - you
-# will recognize it by a sorting error in the MuliBox loss calculation
-
 cudnn.benchmark = True
 
 
 def main():
-    """
-    Training.
-    """
-    global start_epoch, label_map, epoch, checkpoint, decay_lr_at
+    print('Number of classes: ', n_classes)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--checkpoint", type=str, default=None)
+    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--size", type=int, default=300)
+    parser.add_argument("--grad_clip", type=int, default=None)
+    parser.add_argument("--data_folder", type=str, default='./')
+    parser.add_argument("--save_cp", type=str, default='checkpoint300')
+    opt = parser.parse_args()
+
+    assert opt.size in [300, 512]
 
     # Initialize model or load checkpoint
-    if checkpoint is None:
+    if opt.checkpoint is None:
         start_epoch = 0
-        model = SSD300(n_classes=n_classes)
-        # Initialize the optimizer, with twice the default learning rate for biases, as in the original Caffe repo
+        if opt.size == 300:
+            model = SSD300(n_classes=n_classes)
+        else:
+            model = SSD512(n_classes=n_classes)
         biases = list()
         not_biases = list()
         for param_name, param in model.named_parameters():
@@ -53,9 +53,10 @@ def main():
                     not_biases.append(param)
         optimizer = torch.optim.SGD(params=[{'params': biases, 'lr': 2 * lr}, {'params': not_biases}],
                                     lr=lr, momentum=momentum, weight_decay=weight_decay)
+        # Initialize the optimizer, with twice the default learning rate for biases, as in the original Caffe repo
 
     else:
-        checkpoint = torch.load(checkpoint)
+        checkpoint = torch.load(opt.checkpoint)
         start_epoch = checkpoint['epoch'] + 1
         print('\nLoaded checkpoint from epoch %d.\n' % start_epoch)
         model = checkpoint['model']
@@ -65,17 +66,17 @@ def main():
     model = model.to(device)
     criterion = MultiBoxLoss(priors_cxcy=model.priors_cxcy).to(device)
 
-    # Custom dataloaders
-    train_dataset = PascalVOCDataset(data_folder,
-                                     split='train')
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
+    # train_dataset = PascalVOCDataset(opt.data_folder, dim=(opt.size, opt.size))
+    train_dataset = COCODataset(opt.data_folder, dim=(opt.size, opt.size))
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True,
                                                collate_fn=train_dataset.collate_fn, num_workers=workers,
-                                               pin_memory=True)  # note that we're passing the collate function here
+                                               pin_memory=True)
 
     # Calculate total number of epochs to train and decay learning rate at (i.e. convert iterations to epochs)
     # To convert iterations to epochs, divide iterations by the number of iterations per epoch
     # The paper trains for 120,000 iterations with a batch size of 32, decays after 80,000 and 100,000 iterations
     epochs = iterations // (len(train_dataset) // 32)
+    decay_lr_at = [80000, 100000]
     decay_lr_at = [it // (len(train_dataset) // 32) for it in decay_lr_at]
 
     # Epochs
@@ -90,22 +91,14 @@ def main():
               model=model,
               criterion=criterion,
               optimizer=optimizer,
-              epoch=epoch)
+              epoch=epoch,
+              grad_clip=opt.grad_clip)
 
         # Save checkpoint
-        save_checkpoint(epoch, model, optimizer, 'checkpoint300')
+        save_checkpoint(epoch, model, optimizer, opt.save_cp)
 
 
-def train(train_loader, model, criterion, optimizer, epoch):
-    """
-    One epoch's training.
-
-    :param train_loader: DataLoader for training data
-    :param model: model
-    :param criterion: MultiBox loss
-    :param optimizer: optimizer
-    :param epoch: epoch number
-    """
+def train(train_loader, model, criterion, optimizer, epoch, grad_clip=None):
     model.train()  # training mode enables dropout
 
     batch_time = AverageMeter()  # forward prop. + back prop. time
@@ -153,7 +146,8 @@ def train(train_loader, model, criterion, optimizer, epoch):
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(epoch, i, len(train_loader),
                                                                   batch_time=batch_time,
                                                                   data_time=data_time, loss=losses))
-    del predicted_locs, predicted_scores, images, boxes, labels  # free some memory since their histories may be stored
+    del predicted_locs, predicted_scores, images, boxes, labels
+    # free some memory
 
 
 if __name__ == '__main__':
